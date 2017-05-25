adba214fc344bbd342beb0824b896fa3015d509b
mendelB <mendelblesofsky@gmail.com>
2017-05-25 17:17:41 -0400

    add fake image and news info

--- src/App.js
+++ src/App.js
@@ -1,18 +1,20 @@
 import React, { Component } from 'react';
 import { Thumbnail, Button } from 'react-bootstrap';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <Thumbnail className="logo" src={logo} alt="242x200">
-          <h3>Thumbnail label</h3>
-          <p>Description</p>
+        <Thumbnail className="logo" src="https://cdn1.tnwcdn.com/wp-content/blogs.dir/1/files/2017/05/Magikarp-Jump.jpg" alt="242x200">
+          <h3>Magikarp Jump, the latest mobile Pokémon game, is out today</h3>
+          <p>The Pokémon Company just released Magikarp Jump,
+            a mobile game starring the world's least-loved finned Pokémon.
+            To use the developer's own words, \"What is this world coming ...</p>
           <p>
-            <Button bsStyle="primary">Button</Button>&nbsp;
-            <Button bsStyle="default">Button</Button>
+            <a 
+              href="https://thenextweb.com/gaming/2017/05/25/magikarp-jump-latest-mobile-pokemon-game-today/">
+                <Button bsStyle="primary">Full Article</Button></a>
           </p>
         </Thumbnail>
       </div>
