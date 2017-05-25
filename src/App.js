d08b6100974f915c7a7c90157051116e307a94b1
mendelB <mendelblesofsky@gmail.com>
2017-05-25 17:02:24 -0400

    add basic thumbnail template

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { Thumbnail, Button } from 'react-bootstrap';
 import logo from './logo.svg';
 import './App.css';
 
@@ -6,7 +7,14 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-
+        <Thumbnail className="logo" src={logo} alt="242x200">
+          <h3>Thumbnail label</h3>
+          <p>Description</p>
+          <p>
+            <Button bsStyle="primary">Button</Button>&nbsp;
+            <Button bsStyle="default">Button</Button>
+          </p>
+        </Thumbnail>
       </div>
     );
   }
