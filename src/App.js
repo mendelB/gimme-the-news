868f99decd768483721ea955eae03e8477c0cb32
mendelB <mendelblesofsky@gmail.com>
2017-05-26 15:11:49 -0400

    be rid of all logic save for rendering sources bar

--- src/App.js
+++ src/App.js
@@ -1,30 +1,13 @@
 import React, { Component } from 'react';
-import { Thumbnail, Button, Col } from 'react-bootstrap';
-import HeadlineRenderer from './components/HeadlineRenderer';
 import SourcesBar from './components/SourcesBar';
 import './App.css';
 
-const sources = ["the-next-web", "bbc-news", "bloomberg"]
 
 class App extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      activeKey: 1
-    }
-
-    this.changeSource = this.changeSource.bind(this);
-  }
-
-  changeSource(key) {
-    this.setState({activeKey: Number(key)})
-  }
-
   render() {
     return (
       <div>
-        <SourcesBar changeSource={this.changeSource}/>
-        <HeadlineRenderer newsSource={sources[this.state.activeKey - 1]}/>
+        <SourcesBar/>
       </div>
     );
   }
