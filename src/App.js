5790f2454f1903607bf49b7a59333ac0c8e34d7e
mendelB <mendelblesofsky@gmail.com>
2017-05-26 14:14:12 -0400

    remove div wrapper

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import React, { Component } from 'react';
-import { Thumbnail, Button } from 'react-bootstrap';
+import { Thumbnail, Button, Col } from 'react-bootstrap';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
+      <Col md={4} xs={6} className="App">
         <Thumbnail className="news-thumbnail" src="https://cdn1.tnwcdn.com/wp-content/blogs.dir/1/files/2017/05/Magikarp-Jump.jpg" alt="242x200">
           <h3>Magikarp Jump, the latest mobile Pokémon game, is out today</h3>
           <p>The Pokémon Company just released Magikarp Jump,
@@ -18,7 +18,7 @@ class App extends Component {
                 <Button bsStyle="primary">Full Article</Button></a>
           </p>
         </Thumbnail>
-      </div>
+      </Col>
     );
   }
 }
