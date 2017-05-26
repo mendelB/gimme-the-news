f8ce790fee2e51fe6ca267ab157b5c072bf62cff
mendelB <mendelblesofsky@gmail.com>
2017-05-26 15:12:08 -0400

    move workload to source bar based on sidebar tabbing

--- src/components/SourcesBar.js
+++ src/components/SourcesBar.js
@@ -1,6 +1,9 @@
 import React, { Component } from 'react';
-import { Nav, NavItem,  } from 'react-bootstrap';
+import HeadlineRenderer from './HeadlineRenderer';
+import { Nav, NavItem, Tab, Row, Col  } from 'react-bootstrap';
 import '../App.css';
+
+const sources = ["the-next-web", "bbc-news", "bloomberg"]
 
 class SourcesBar extends Component {
   constructor(props) {
@@ -15,16 +18,29 @@ class SourcesBar extends Component {
 
   handleSelect(eventKey) {
     this.setState({activeKey: eventKey})
-    this.props.changeSource(eventKey)
   }
 
   render() {
     return (
-      <Nav bsStyle="tabs" activeKey={this.state.activeKey} onSelect={this.handleSelect}>
-        <NavItem eventKey="1" href="/home">NavItem 1 content</NavItem>
-        <NavItem eventKey="2" title="Item">NavItem 2 content</NavItem>
-        <NavItem eventKey="3">NavItem 3 content</NavItem>
-      </Nav>
+      <div>
+      <Tab.Container id="left-tabs-example" defaultActiveKey="1" onSelect={this.handleSelect}>
+        <Row className="clearfix">
+          <Col sm={2}>
+            <Nav bsStyle="pills" stacked>
+              <NavItem eventKey="1">
+                Tab 1
+              </NavItem>
+              <NavItem eventKey="2">
+                Tab 2
+              </NavItem>
+            </Nav>
+          </Col>
+          <Col sm={10}>
+            <HeadlineRenderer newsSource={sources[Number(this.state.activeKey) - 1]}/>
+          </Col>
+        </Row>
+    </Tab.Container>
+    </div>
     );
   }
 }
