2d1142409bafa465382beb0482e2c5b02eb6cc7a
mendelB <mendelblesofsky@gmail.com>
2017-05-26 15:02:18 -0400

    based on selected tab, source changes

--- src/App.js
+++ src/App.js
@@ -1,14 +1,30 @@
 import React, { Component } from 'react';
 import { Thumbnail, Button, Col } from 'react-bootstrap';
 import HeadlineRenderer from './components/HeadlineRenderer';
+import SourcesBar from './components/SourcesBar';
 import './App.css';
 
+const sources = ["the-next-web", "bbc-news", "bloomberg"]
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      activeKey: 1
+    }
+
+    this.changeSource = this.changeSource.bind(this);
+  }
+
+  changeSource(key) {
+    this.setState({activeKey: Number(key)})
+  }
+
   render() {
     return (
       <div>
-        <HeadlineRenderer newsSource="the-next-web"/>
+        <SourcesBar changeSource={this.changeSource}/>
+        <HeadlineRenderer newsSource={sources[this.state.activeKey - 1]}/>
       </div>
     );
   }
