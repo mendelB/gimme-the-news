3c194f38d9760033014747dd190fcdc0f2464b36
mendelB <mendelblesofsky@gmail.com>
2017-05-26 14:20:32 -0400

    change data to props

--- src/components/NewsHeadline.js
+++ src/components/NewsHeadline.js
@@ -6,15 +6,13 @@ class NewsHeadline extends Component {
   render() {
     return (
       <Col md={4} xs={6} className="App">
-        <Thumbnail className="news-thumbnail" src="https://cdn1.tnwcdn.com/wp-content/blogs.dir/1/files/2017/05/Magikarp-Jump.jpg" alt="242x200">
-          <h3>Magikarp Jump, the latest mobile Pokémon game, is out today</h3>
-          <p>The Pokémon Company just released Magikarp Jump,
-            a mobile game starring the world's least-loved finned Pokémon.
-            To use the developer's own words, \"What is this world coming ...</p>
+        <Thumbnail className="news-thumbnail" src={this.props.image} alt="242x200">
+          <h3>{this.props.headline}</h3>
+          <p>{this.props.description}</p>
           <p>
             <a 
               target="_blank"
-              href="https://thenextweb.com/gaming/2017/05/25/magikarp-jump-latest-mobile-pokemon-game-today/">
+              href={this.props.articleLink}>
                 <Button bsStyle="primary">Full Article</Button></a>
           </p>
         </Thumbnail>
