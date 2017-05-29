2054f30a1e3fff0609f0f7611415f630c0e3d863
mendelB <mendelblesofsky@gmail.com>
2017-05-29 19:49:53 -0400

    hook component with dynamic api fetching instead of dummy data

--- src/components/HeadlineRenderer.js
+++ src/components/HeadlineRenderer.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import NewsHeadline from './NewsHeadline';
+import newsFetcher from '../lib/NewsFetcher'
 import '../App.css';
 
 let articles = { "the-next-web" : [
@@ -251,10 +252,28 @@ let articles = { "the-next-web" : [
 }
 
 class HeadlineRenderer extends Component {
+  constructor(props) {
+    super(props);
+    
+    this.state = {
+      news: []
+    }
+  }
+
+  componentDidMount() {
+    newsFetcher.getNews(this.props.newsSource)
+    .then(news => this.setState({news: news}))
+  }
+
+  componentWillReceiveProps(nextProps) {
+    newsFetcher.getNews(nextProps.newsSource)
+    .then(news => this.setState({news: news})) 
+  }
+
   render() {
     return (
       <div>
-        {articles[this.props.newsSource].map(article => <NewsHeadline image={article.urlToImage} headline={article.title} description={article.description} articleLink={article.url}/>)}
+        {this.state.news.map(article => <NewsHeadline image={article.urlToImage} headline={article.title} description={article.description} articleLink={article.url}/>)}
       </div>
     );
   }
