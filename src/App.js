c5c095caeda4884bf9ad8f5f5ed750831cefb7c0
mendelB <mendelblesofsky@gmail.com>
2017-06-07 13:47:13 -0400

    add editing state

--- src/App.js
+++ src/App.js
@@ -1,13 +1,38 @@
 import React, { Component } from 'react';
 import SourcesBar from './components/SourcesBar';
+import EditSources from './components/EditSources';
+import { Navbar, Nav, NavItem  } from 'react-bootstrap';
 import './App.css';
 
 
 class App extends Component {
+	constructor(props) {
+		super(props);
+		this.state = {
+			editing: false
+		}
+		this.editSources = this.editSources.bind(this)
+	}
+
+	editSources(e) {
+		e.preventDefault()
+		this.setState({editing: true})
+	}
+
   render() {
     return (
       <div>
-        <SourcesBar/>
+      	<Navbar>
+			    <Navbar.Header>
+			      <Navbar.Brand>
+			        <a href="#">React-Bootstrap</a>
+			      </Navbar.Brand>
+			    </Navbar.Header>
+			    <Nav>
+			      <NavItem eventKey={1} onClick={this.editSources}>Edit News Sources</NavItem>
+			    </Nav>
+			  </Navbar>
+        {this.state.editing ? <EditSources/> : <SourcesBar/>}
       </div>
     );
   }
