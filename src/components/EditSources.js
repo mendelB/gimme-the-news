ed135669f7715ee888846d034558a53251b3ac1c
mendelB <mendelblesofsky@gmail.com>
2017-06-07 13:47:00 -0400

    add editsources component

---
+++ src/components/EditSources.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import HeadlineRenderer from './HeadlineRenderer';
+import newsFetcher from '../lib/NewsFetcher'
+import { Nav, NavItem, Tab, Row, Col, FormGroup, Checkbox  } from 'react-bootstrap';
+import '../App.css';
+
+class EditSources extends Component {
+  render() {
+    return (
+      <div>
+      </div>
+    );
+  }
+}
+
+export default EditSources;
