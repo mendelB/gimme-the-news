31620c2ab4019d427af946918a7f8b97d8627c4a
mendelB <mendelblesofsky@gmail.com>
2017-06-08 15:58:54 -0400

    functionality for editing list of my sources

--- src/components/EditSources.js
+++ src/components/EditSources.js
@@ -9,9 +9,11 @@ class EditSources extends Component {
     super(props);
 
     this.state = {
-      sources: []
+      sources: [],
+      mySources: newsFetcher.mySources()
     }
 
+    this.handleChange = this.handleChange.bind(this)
   }
 
 	componentDidMount() {
@@ -19,14 +21,26 @@ class EditSources extends Component {
     .then(sources => this.setState({sources: sources}))
   }
 
+  handleChange(e) {
+  	let source = this.state.sources[Number(e.target.value)]
+  	let mySources
+  	if (e.target.checked) {
+  		mySources = this.state.mySources.concat(source)
+  	} else {
+  		mySources = this.state.mySources.filter(s => s!== source)
+  	}
+  	this.setState({mySources: mySources})
+  	newsFetcher.updateMySources(mySources)
+  }
+
   render() {
     return (
-      <ul>
-      	{ this.state.sources.map( source => {
-      			return <li>{source.name}</li>
+      <form>
+      	{ this.state.sources.map( (source, index) => {
+      			return <Checkbox value={index} checked={this.state.mySources.includes(source) ? true : false} onChange={this.handleChange}>{source.name}</Checkbox>
       		})
       	}
-      </ul>
+      </form>
     );
   }
 }
