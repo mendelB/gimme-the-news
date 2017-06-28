a54d731e9349e52c16376e1482fd5947433457ed
mendelB <mendelblesofsky@gmail.com>
2017-06-28 15:58:14 -0400

    turn into function

--- src/components/NewsHeadline.js
+++ src/components/NewsHeadline.js
@@ -2,23 +2,21 @@ import React, { Component } from 'react';
 import { Thumbnail, Button, Col } from 'react-bootstrap';
 import '../App.css';
 
-class NewsHeadline extends Component {
-  render() {
-    return (
-      <Col md={4} xs={6} className="news-headline">
-        <Thumbnail className="news-thumbnail" src={this.props.image} alt="242x200">
-          <h3>{this.props.headline}</h3>
-          <p className="description">{this.props.description.split(" ").splice(0, 13).join(" ")+ "..."}</p>
-          <p>
-            <a 
-              target="_blank"
-              href={this.props.articleLink}>
-                <Button bsStyle="primary">Full Article</Button></a>
-          </p>
-        </Thumbnail>
-      </Col>
-    );
-  }
+function NewsHeadline(props) {
+  return (
+    <Col md={4} xs={6} className="news-headline">
+      <Thumbnail className="news-thumbnail" src={props.image} alt="242x200">
+        <h3>{props.headline}</h3>
+        <p className="description">{props.description.split(" ").splice(0, 13).join(" ")+ "..."}</p>
+        <p>
+          <a 
+            target="_blank"
+            href={props.articleLink}>
+              <Button bsStyle="primary">Full Article</Button></a>
+        </p>
+      </Thumbnail>
+    </Col>
+  );
 }
 
 export default NewsHeadline;
