0adbd9437094ba4ddc3b13f20f180c7c372fc4fd
mendelB <mendelblesofsky@gmail.com>
2017-06-28 16:16:27 -0400

    space

--- src/lib/NewsFetcher.js
+++ src/lib/NewsFetcher.js
@@ -6,6 +6,7 @@ const SOURCES_URL = BASE + "/sources?"
 const NEWS = BASE + "/articles?source="
 const API_TAIL = "&apiKey=" + process.env.REACT_APP_API_KEY
 
+
 let configSources = sources => {
 	sources.forEach(source => {
 		let sourceObj = { name: source.name, id: source.id, favored: false }
