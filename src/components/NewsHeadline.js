eb046076e9efc1b3edecd2b125ece6a3dc82509a
mendelB <mendelblesofsky@gmail.com>
2017-06-29 23:03:22 -0400

    styling for headline - headline one big link to article in place of button

--- src/components/NewsHeadline.js
+++ src/components/NewsHeadline.js
@@ -4,12 +4,12 @@ import '../App.css';
 function NewsHeadline(props) {
   return (
     <div className="col-md-6 col-lg-4 col-xs-12 news-headline">
-      <div className="card text-center">
+      <div className="card card-outline-info text-center">
+        <a target="_blank" href={props.articleLink} className="article-link"></a>
         <img className="card-img-top news-thumbnail" src={props.image} alt="Card image cap"/>
         <div className="card-block">
-          <h4 className="card-title">{props.headline}</h4>
+          <h4 className="card-title headline">{props.headline}</h4>
           <p className="card-text">{props.description.split(" ").splice(0, 13).join(" ")+ "..."}</p>
-          <a target="_blank" href={props.articleLink} className="btn btn-primary">Full Article</a>
         </div>
       </div>
     </div>
